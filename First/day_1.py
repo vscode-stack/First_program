213e52167884017584ed523aa41255ec09524981
V.S <vibhutik665@gmail.com>
2025-11-19 19:39:19 +0530

    Add files via upload

---
+++ First/day_1.py
@@ -0,0 +1,28 @@
+#Day 1: Licence check
+#using variables, lists, conditionals, for loops 
+
+'''I have to make one licence check for driving licence where my program check 
+licence using given input . 
+
+'''
+
+print("Driving licence checker\n-------------------------")
+
+age = int(input('Enter your age:'))
+
+if(age > 18):
+    print("Your age is eligible for driving licence.")
+
+elif(age < 18):
+    print("Your age is not eligible for driving licence.")
+
+else:
+    print("Please visit our national licence eligibility counciler")
+
+
+    
+
+
+ 
+ 
+
